27a85efd8da57b34790d9c8394ec1505ea9a644c
Abhishek Sunandan <absunandan008@gmail.com>
2026-05-03 16:10:21 -0400

    add configs

---
+++ app/config.py
@@ -0,0 +1,8 @@
+EMBEDDING_MODEL = "all-MiniLM-L6-v2"
+OLLAMA_MODEL = "llama3.2"
+OLLAMA_URL = "http://localhost:11434/api/generate"
+CHROMA_PATH = "/.vector_store"
+COLLECTION_NAME = "rag_base"
+CHUNK_SIZE = 300
+CHUNK_OVERLAP = 50
+TOP_K = 3
